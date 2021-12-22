9b9a651ebfd51418ee21a5ea99dd6e2ba5ef7bae
ryan10812 <90675171+ryan10812@users.noreply.github.com>
2021-12-22 09:59:02 +0800

    321

---
+++ js教學.js
@@ -0,0 +1,15 @@
+// js基礎
+
+// 1.以分號; 斷句
+alert('132');
+
+// 2.js會自動省略空白，以下兩者完全相等
+alert('123')
+alert ( '123' )
+
+// 建立一個名為 abc的函式
+function abc(){
+    // code here
+}
+// 建立一個名為 abc 的變數
+var abc 
