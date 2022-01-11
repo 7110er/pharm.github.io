422ff722f74ad3d49594279f0b06c4238510f43f
Ryan <ryan10812@gmail.com>
2022-01-11 14:52:19 +0800

    123

---
+++ my-app/src/page/Home/index.js
@@ -0,0 +1,16 @@
+import Edit from "./components/Edit"
+import Item from "./components/Item";
+const Home = () => {
+    return <div>
+    <div className="homeBar" id="homeBar">
+      <a  id="drugBtn"><i className="fa fa-search"></i>&nbsp;藥品</a>
+      <a  id="menuBtn"><i className='fa fa-question-circle'></i>&nbsp;常見問題</a>
+      <a  id="navBtn" ><i className="fa fa-bars"></i>&nbsp;衛教</a>
+    </div>
+    <br></br>
+    <br></br>
+    <br></br>
+
+    </div>
+}
+export default Home;
